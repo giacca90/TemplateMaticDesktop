582c3568e2f3fd96e50aaab130ddcd269077190d
Giacca90 <giacca90mobile@gmail.com>
2024-01-07 02:57:04 +0100

    eliminada barra de menu

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('node:path')
 
 function createWindow () {
@@ -42,3 +42,6 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+//const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(null)
