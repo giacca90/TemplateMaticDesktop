a93be4555e998921bfc7b544878add6ccde5f7eb
Giacca90 <giacca90mobile@gmail.com>
2024-01-17 03:27:30 +0100

    Limpieza de codigo obsoleto

--- ng/src/app/agregar/agregar.component.ts
+++ ng/src/app/agregar/agregar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Cliente, ClientesService, ClienteDinamico } from '../services/clientes.service';
+import { ClientesService, ClienteDinamico } from '../services/clientes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -8,14 +8,11 @@ import { Cliente, ClientesService, ClienteDinamico } from '../services/clientes.
   templateUrl: './agregar.component.html',
   styleUrl: './agregar.component.css',
 })
-export class AgregarComponent implements OnInit{
+export class AgregarComponent implements OnInit {
   CSV: File;
-  //clientes:Cliente[] = this.CS.clientes;
-  clientes:ClienteDinamico[] = [];
+  clientes: ClienteDinamico[] = [];
 
-  constructor(private CS: ClientesService) {
-
-  }
+  constructor(private CS: ClientesService) {}
   ngOnInit(): void {
     this.clientes = this.CS.clientes;
   }
@@ -25,44 +22,21 @@ export class AgregarComponent implements OnInit{
     let reader = new FileReader();
     reader.readAsText(this.CSV);
     reader.onload = () => {
-      let CSVString:string = reader.result.toString();
-//      console.log("CSVString: "+CSVString);
-      let rows:string[] = CSVString.split('\n');
-//      let count = 0;
-      let atributos:string[];
-      for(let i=0; i<rows.length; i++) {
+      let CSVString: string = reader.result.toString();
+      let rows: string[] = CSVString.split('\n');
+      let atributos: string[];
+      for (let i = 0; i < rows.length; i++) {
         let cliente: ClienteDinamico;
-//        console.log("row "+i+": "+ rows[i]);
         let val = rows[i].split(',');
-        if(i===0) {
+        if (i === 0) {
           atributos = val;
-        }else{
+        } else {
           cliente = new ClienteDinamico(atributos);
           cliente.addValores(val, i);
           this.CS.clientes.push(cliente);
-        } 
+        }
       }
       this.clientes = this.CS.clientes;
-
-      /* Versión con Cliente */
-      /* rows.forEach((row) => {
-        console.log("raw: "+row)
-        let val = row.split(',');
-        console.log("val[0]: "+val[0]);
-        let cliente = new Cliente(
-          count,
-          val[0],
-          val[1],
-          val[2],
-          val[3],
-          val[4],
-          val[5],
-          val[6]
-        );
-        count++;
-        this.CS.addCliente(cliente);
-      }); */
-    //  this.clientes = this.CS.clientes;
     };
   }
 }
--- ng/src/app/services/clientes.service.ts
+++ ng/src/app/services/clientes.service.ts
@@ -4,13 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ClientesService {
-  //public clientes:Array<Cliente> = [];
   public clientes:Array<ClienteDinamico> = [];
   constructor() { }
-
- /*  addCliente(cliente:Cliente) {
-    this.clientes.push(cliente);
-  } */
 
   addCliente(cliente:ClienteDinamico) {
     this.clientes.push(cliente);
@@ -23,32 +18,6 @@ export class ClientesService {
       }
     }
     return null;
-  }
-}
-
-export class Cliente {
-  id:number;
-  nombre:string;
-  apellido:string;
-  nombreREML:string;
-  numeroREML:string;
-  direccion:string;
-  correo:string;
-  fechaNacimiento:string;
-
-  constructor(_id:number, _nombre:string, _apellido:string, _nombreREML:string, _numeroREML:string, _direccion:string, _correo:string, _fechaNacimiento:string) {
-    this.id = _id;
-    this.nombre = _nombre;
-    this.apellido = _apellido;
-    this.nombreREML = _nombreREML;
-    this.numeroREML = _numeroREML;
-    this.direccion = _direccion;
-    this.correo = _correo;
-    this.fechaNacimiento = _fechaNacimiento;
-  }
-
-  toString(): string {
-    return "Nombre: "+this.nombre+" Apellido: "+this.apellido+" Nombre REML: "+this.nombreREML+" Numero REML: "+this.numeroREML+" Dirección: "+this.direccion+" Correo Electronico: "+this.correo+" Fecha de nacimiento: "+this.fechaNacimiento
   }
 }
 
