6e1560abce698b73232e8b6535c0938add8b9e7d
Giacca90 <giacca90mobile@gmail.com>
2024-01-23 00:37:08 +0100

    Modificado componente Home para que funcione el Web y en Electron

--- ng/src/app/home/home.component.ts
+++ ng/src/app/home/home.component.ts
@@ -20,46 +20,89 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     if (this.PS.getTemp()) {
       this.plantillasBuscadas = this.PS.getTemp();
-    }else{
-      let plantillas:Plantilla[] = [];
-      this.ipcRenderer.send("PersistenciaCarpeta");
-      this.ipcRenderer.on("Carpeta", (_event, files:string[]) => {
-        for(let i=0; i<files.length; i++) {
-          console.log("Prueba: "+files[i]);
-          if(files[i].endsWith("odt") || files[i].endsWith("docx")) 
-          plantillas.push(new Plantilla(i+1, null, files[i].split('/').slice(-1)[0], files[i]));
-        }
-        this.PS.setTemp(plantillas);
-        this.plantillasBuscadas = plantillas;
-        this.cdr.detectChanges();
-      })
-    }
-    let input= document.getElementById('input') as HTMLInputElement;
-    if(input) {
-      input.addEventListener('change', () => {
+    } 
+    if(this.ipcRenderer.isElectron()) { 
+      let selector = document.createElement("button");
+      selector.innerText = "Elige una carpeta"
+      selector.addEventListener("click", () => this.abreDialog())
+//      selector.click = () => this.abreDialog();
+      let dom = document.getElementById("selector");
+      dom.appendChild(selector);
+    } else {
+      let selector = document.createElement("input");
+      selector.type = "file";
+      selector.name = "plantillas";
+      selector.webkitdirectory = true;
+      selector.multiple = true;
+      selector.onchange = () => {
         this.PS.setTemp([]);
-        if (input.files) {  
-          let plantillas:Plantilla[] = [];
-          for (let i = 0; i < input.files.length; i++) {
-            if (
-              input.files[i].name.endsWith('odt') ||
-              input.files[i].name.endsWith('docx')
-            ) {
-              plantillas.push(new Plantilla(i + 1, input.files[i]));
-              let reader = new FileReader();
-              reader.readAsDataURL(input.files[i]);
-              reader.onload = () => {
-                this.ipcRenderer.send("Files", reader.result);              
+          if (selector.files) {  
+            let plantillas:Plantilla[] = [];
+            for (let i = 0; i < selector.files.length; i++) {
+              if (
+                selector.files[i].name.endsWith('odt') ||
+                selector.files[i].name.endsWith('docx')
+              ) {
+                plantillas.push(new Plantilla(i + 1, selector.files[i]));
+                /* let reader = new FileReader();
+                reader.readAsDataURL(selector.files[i]);
+                reader.onload = () => {
+                  this.ipcRenderer.send("Files", reader.result);              
+                } */
               }
             }
+            this.PS.setTemp(plantillas);
+            this.plantillasBuscadas = plantillas;
+            this.cdr.detectChanges();
+          }
+      }
+      let dom = document.getElementById('selector');
+      dom.appendChild(selector);
+    }
+         /* if (this.PS.getTemp()) {
+        this.plantillasBuscadas = this.PS.getTemp();
+      }else{
+        let plantillas:Plantilla[] = [];
+        this.ipcRenderer.send("PersistenciaCarpeta");
+        this.ipcRenderer.on("Carpeta", (_event, files:string[]) => {
+          for(let i=0; i<files.length; i++) {
+//            console.log("Prueba: "+files[i]);
+            if(files[i].endsWith("odt") || files[i].endsWith("docx")) 
+            plantillas.push(new Plantilla(i+1, null, files[i].split('/').slice(-1)[0], files[i]));
           }
           this.PS.setTemp(plantillas);
           this.plantillasBuscadas = plantillas;
           this.cdr.detectChanges();
-        }
-      });
-    }
-    
+        })
+      }
+    }else{
+      let input = document.getElementById('input') as HTMLInputElement;
+      console.log("Elemento input: "+input);
+      if(input !== null) {
+        input.addEventListener('change', () => {
+          this.PS.setTemp([]);
+          if (input.files) {  
+            let plantillas:Plantilla[] = [];
+            for (let i = 0; i < input.files.length; i++) {
+              if (
+                input.files[i].name.endsWith('odt') ||
+                input.files[i].name.endsWith('docx')
+              ) {
+                plantillas.push(new Plantilla(i + 1, input.files[i]));
+                let reader = new FileReader();
+                reader.readAsDataURL(input.files[i]);
+                reader.onload = () => {
+                  this.ipcRenderer.send("Files", reader.result);              
+                }
+              }
+            }
+            this.PS.setTemp(plantillas);
+            this.plantillasBuscadas = plantillas;
+            this.cdr.detectChanges();
+          }
+        });
+      }    
+    } */
   }
 
   busca() {
