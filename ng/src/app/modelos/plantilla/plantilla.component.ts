564d3cda398fbdb94f2b91fac96cbb3221b722e6
Giacca90 <giacca90mobile@gmail.com>
2024-01-22 12:22:44 +0100

    Comprobado comportamiento correcto de sustitución en header y footer

--- ng/src/app/modelos/plantilla/plantilla.component.ts
+++ ng/src/app/modelos/plantilla/plantilla.component.ts
@@ -1,8 +1,11 @@
-import { Component, inject, ChangeDetectorRef } from '@angular/core';
+import { Component, inject, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Plantilla, PlantillaService } from '../../services/plantilla.service';
-import { ClienteDinamico, ClientesService} from '../../services/clientes.service';
-import { IpcService } from '../../services/ipc-render.service'
+import {
+  ClienteDinamico,
+  ClientesService,
+} from '../../services/clientes.service';
+import { IpcService } from '../../services/ipc-render.service';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import * as file2html from 'file2html';
@@ -21,7 +24,7 @@ file2html.config({
   templateUrl: './plantilla.component.html',
   styleUrl: './plantilla.component.css',
 })
-export class PlantillaComponent {
+export class PlantillaComponent implements OnDestroy{
   route: ActivatedRoute = inject(ActivatedRoute);
   id: number;
   file: File;
@@ -35,17 +38,23 @@ export class PlantillaComponent {
   progresoCargaInicial: number = 0;
   estadoCreacionArchivo: boolean = false;
   progresoCreacionArchivo: number = 0;
+  worker: Worker
 
-  constructor(public PS: PlantillaService, public CS: ClientesService, IPC: IpcService, private cdr: ChangeDetectorRef) {
+  constructor(
+    public PS: PlantillaService,
+    public CS: ClientesService,
+    IPC: IpcService,
+    private cdr: ChangeDetectorRef
+  ) {
     IPC.clear();
     this.id = this.route.snapshot.queryParams['id'];
     console.log('id: ' + this.id);
-    let plantilla:Plantilla = PS.getPlantillaForId(this.id);
+    let plantilla: Plantilla = PS.getPlantillaForId(this.id);
     this.file = plantilla.file;
-    if(this.file === null) {
-      IPC.send("busca", plantilla.address);
-      IPC.on("arraybuffer", (_event, arraybuffer:ArrayBuffer) => {
-        this.file = new File ([arraybuffer], plantilla.nombre);
+    if (this.file === null) {
+      IPC.send('busca', plantilla.address);
+      IPC.on('arraybuffer', (_event, arraybuffer: ArrayBuffer) => {
+        this.file = new File([arraybuffer], plantilla.nombre);
         this.nuevoFile = this.file;
         this.nombre = plantilla.nombre;
         console.log('nombre: ' + this.nombre);
@@ -54,19 +63,22 @@ export class PlantillaComponent {
 
         this.abrirArchivo();
       });
-    }else{
+    } else {
       this.abrirArchivo();
     }
   }
+  ngOnDestroy(): void {
+    this.worker.terminate()
+  }
 
-  async abrirArchivo() {;
+  async abrirArchivo() {
     // Descomprime el archivo
     const zip = await JSZip().loadAsync(this.file);
     // Obtener la lista de archivos
     const archivos = Object.keys(zip.files);
     // Procesar cada archivo
-    for(let i=0; i<archivos.length; i++) { 
-      if(archivos[i].endsWith('xml')) {
+    for (let i = 0; i < archivos.length; i++) {
+      if (archivos[i].endsWith('xml')) {
         const contenido = await zip.file(archivos[i]).async('text');
         // Ahora puedes procesar el contenido XML como desees
         const parser = new DOMParser();
@@ -77,12 +89,12 @@ export class PlantillaComponent {
       }
       this.progresoCargaInicial = ((i + 1) / archivos.length) * 100;
       this.cdr.detectChanges();
-    };
+    }
 
-    if(this.file.name.endsWith('odt')) {
+    if (this.file.name.endsWith('odt')) {
       this.vistaOdt();
     }
-    if(this.file.name.endsWith('docx')) {
+    if (this.file.name.endsWith('docx')) {
       this.vistaDocx();
     }
   }
@@ -91,14 +103,14 @@ export class PlantillaComponent {
     let vista = null;
     const zip = await JSZip().loadAsync(this.file);
     const reader = new FileReader();
-    vista = await zip.file("Thumbnails/thumbnail.png").async('blob');
+    vista = await zip.file('Thumbnails/thumbnail.png').async('blob');
     reader.readAsDataURL(vista);
     reader.onload = () => {
       let view = document.getElementById('contentContainer');
       // Crea un elemento de imagen y establece su fuente como los datos de la imagen
       const imgElement = document.createElement('img');
-      imgElement.src = reader.result as string
-      imgElement.alt = "Cargando..."
+      imgElement.src = reader.result as string;
+      imgElement.alt = 'Cargando...';
       // Establece el ancho del elemento img al ancho del div
       imgElement.style.width = '100%';
       // Agrega la imagen al div
@@ -109,54 +121,24 @@ export class PlantillaComponent {
   }
 
   async vistaDocx() {
-
     if (typeof Worker !== 'undefined') {
       // Create a new
-      const worker = new Worker(new URL('./vista-docx.worker', import.meta.url));
-      worker.postMessage(this.file);
-      worker.onmessage = ({ data }) => {
+      this.worker = new Worker(
+        new URL('./vista-docx.worker', import.meta.url)
+      );
+      this.worker.postMessage(this.file);
+      this.worker.onmessage = ({ data }) => {
         let view = document.getElementById('contentContainer');
-        view.innerHTML = '<div id="contenido" style="width: 100%; height: 100%; overflow: hidden;">'+data+'</div>';
-    
+        view.innerHTML =
+          '<div id="contenido" style="width: 100%; height: 100%; overflow: hidden;">' +
+          data +
+          '</div>';
       };
     } else {
       let view = document.getElementById('contentContainer');
-      view.innerHTML = '<h3 text-color: red>NO SE PUEDE CARGAR UNA VISTA PREVIA!!!</h3>';
-    
+      view.innerHTML =
+        '<h3 text-color: red>NO SE PUEDE CARGAR UNA VISTA PREVIA!!!</h3>';
     }
-    /* //prueba con file2html
-    try {
-      // Espera a que se resuelva la Promesa y obtén el contenido del archivo en formato ArrayBuffer
-      const content = await this.file.arrayBuffer();
-
-      // Lee el archivo y conviértelo a HTML
-      const fileData = await file2html.read({
-        fileBuffer: content,
-        meta: {
-          mimeType:
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        },
-      });
-
-      // Extrae los estilos y el contenido del archivo
-      const { styles, content: fileContent } = fileData.getData();
-
-      // Concatena estilos y contenido
-      const html = styles + fileContent;
-
-//      console.log('RESULTADO: \n' + html);
-
-      let view = document.getElementById('contentContainer');
-      view.innerHTML = '<div id="contenido" style="width: 100%; height: 100%; overflow: hidden;">'+html+'</div>';
-    } catch (error) {
-      // Maneja cualquier error que pueda ocurrir durante el proceso
-      console.error('Error:', error);
-
-      // Imprime información adicional sobre el error específico
-      if (error.code === 'file2html.errors.unsupportedFile') {
-        console.error('El formato del archivo no es compatible.');
-      }
-    } */
   }
 
   buscaClaves(fileString: string) {
@@ -184,7 +166,7 @@ export class PlantillaComponent {
   }
 
   async creaDocumento() {
-    console.log("Comienza creaDocumento");
+    console.log('Comienza creaDocumento');
     this.estadoCreacionArchivo = true;
     this.cdr.detectChanges();
     let parejas: Array<{ clave: string; valor: string }> = [];
@@ -200,39 +182,42 @@ export class PlantillaComponent {
     }
 
     // Descomprime el archivo
-    console.log("Comienza a abrir el archivo");
+    console.log('Comienza a abrir el archivo');
     const zip = await JSZip().loadAsync(this.file);
     // Obtener la lista de archivos
-    console.log("Obtiene lista de archivos");
+    console.log('Obtiene lista de archivos');
     const archivos = Object.keys(zip.files);
     // Procesar cada archivo
-    console.log("Comienza a analizar cada archivo");
-    for (let i=0; i< archivos.length; i++) {
-      if(archivos[i].endsWith('xml')) {
+    console.log('Comienza a analizar cada archivo');
+    for (let i = 0; i < archivos.length; i++) {
+      if (archivos[i].endsWith('xml')) {
         const contenido = await zip.file(archivos[i]).async('text');
         // Ahora puedes procesar el contenido XML como desees
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(contenido, 'text/xml');
         const serializer = new XMLSerializer();
         const SxmlDoc = serializer.serializeToString(xmlDoc);
-//        console.log("Archivo:\n"+SxmlDoc);
+        //        console.log("Archivo:\n"+SxmlDoc);
         await this.sustituyeClaves(SxmlDoc, parejas, archivos[i]);
         this.progresoCreacionArchivo = ((i + 1) / archivos.length) * 100;
         this.cdr.detectChanges();
       }
     }
-    console.log("Comienza la descarga del documento modificado");
+    console.log('Comienza la descarga del documento modificado');
     const link = document.createElement('a');
     link.href = URL.createObjectURL(this.nuevoFile);
     link.download = 'rellenado_' + this.file.name; // Puedes cambiar el nombre según el tipo de archivo
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    
   }
 
-  async sustituyeClaves(SxmlDoc: string, parejas: Array<{ clave: string; valor: string }>, nombreArchivo: string) {
-    console.log("Comienza sustituyeClaves");
+  async sustituyeClaves(
+    SxmlDoc: string,
+    parejas: Array<{ clave: string; valor: string }>,
+    nombreArchivo: string
+  ) {
+    console.log('Comienza sustituyeClaves');
     let documento: string = '';
     let index: number = 0;
     let indexTemp: number = 0;
@@ -245,44 +230,49 @@ export class PlantillaComponent {
           clave = clave.replace(/<.*?>/g, '');
           let valor = '';
           parejas.forEach((par) => {
-              if (par.clave === clave) {
-                valor = par.valor;
-              }
-            });
-            documento =
-              documento + SxmlDoc.substring(indexTemp, index) + valor;
-  
-            index = indexEnd;
-            indexTemp = indexEnd + 2;
-          }
+            if (par.clave === clave) {
+              valor = par.valor;
+            }
+          });
+          documento = documento + SxmlDoc.substring(indexTemp, index) + valor;
+
+          index = indexEnd;
+          indexTemp = indexEnd + 2;
         }
       }
-      documento = documento + SxmlDoc.substring(indexTemp);
-  //    console.log('DOCUMENTO: \n\n' + documento);
-      await this.replaceXmlInCopy(this.nuevoFile, documento, nombreArchivo);
     }
+    documento = documento + SxmlDoc.substring(indexTemp);
+    //    console.log('DOCUMENTO: \n\n' + documento);
+    await this.replaceXmlInCopy(this.nuevoFile, documento, nombreArchivo);
+  }
 
-    async replaceXmlInCopy(originalBlob: File, modifiedXml: string, outputPath: string) {
+  async replaceXmlInCopy(
+    originalBlob: File,
+    modifiedXml: string,
+    outputPath: string
+  ) {
     const zip = new JSZip();
-    console.log("Comienza replaceXml");
+    console.log('Comienza replaceXml');
     // Lee el contenido del archivo original
-    const originalZip = await zip.loadAsync(originalBlob)
+    const originalZip = await zip.loadAsync(originalBlob);
     // Sustituye el contenido XML modificado
-    console.log("Sustituye el contenido XML modificado")
+    console.log('Sustituye el contenido XML modificado');
     originalZip.file(outputPath, modifiedXml);
     // Crea el nuevo archivo
-    this.nuevoFile = await originalZip.generateAsync({ type: 'blob' }) as File
-    console.log("crea nuevo archivo");
+    this.nuevoFile = (await originalZip.generateAsync({
+      type: 'blob',
+    })) as File;
+    console.log('crea nuevo archivo');
   }
-  
+
   completa() {
-    if(this.selected) {
+    if (this.selected) {
       const seleccionado = parseInt(this.selected);
       const cliente: ClienteDinamico = this.CS.getClienteForId(seleccionado);
-      console.log("Cliente obtenido: "+cliente.toString());
-      for(let atributo of cliente.atributos) {
-        for(let clave of this.claves) {
-          if(clave === atributo.clave) {
+      console.log('Cliente obtenido: ' + cliente.toString());
+      for (let atributo of cliente.atributos) {
+        for (let clave of this.claves) {
+          if (clave === atributo.clave) {
             let a = document.getElementById(clave) as HTMLInputElement;
             a.value = atributo.valor;
             a.removeAttribute('placeholder');
