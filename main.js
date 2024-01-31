06879388d007c4e9fcff8cab263d9d846b22b265
Giacca90 <giacca90mobile@gmail.com>
2024-02-01 00:43:19 +0100

    Agregada creación de enlace en el escritrio alinstalar en Windows

--- main.js
+++ main.js
@@ -4,6 +4,81 @@ const path = require('node:path');
 const fs = require('fs');
 const Store = require('electron-store');
 const store = new Store();
+const os = require('os');
+
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  console.log("appFolder: "+appFolder);
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  console.log("rootAtomFolder: "+rootAtomFolder);
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  console.log('updateDotExe: '+updateDotExe)
+  const exeName = path.basename(process.execPath);
+  console.log('exeName: '+exeName);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+      const appexe = path.join(appFolder,'template-matic-desktop.exe');
+      const link =path.join(os.homedir(),'Desktop','TemplateMatic.lnk')
+      fs.symlinkSync(appexe, link);
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
 
 function createWindow () {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
