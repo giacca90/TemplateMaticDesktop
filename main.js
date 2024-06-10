5da70cab40204cadb1e0a56b7e50568b4e643cf3
Giacca90 <giacca90mobile@gmail.com>
2024-06-10 02:02:25 +0200

    corrección

--- main.js
+++ main.js
@@ -1,249 +1,252 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu, ipcMain, dialog, screen, globalShortcut } = require("electron");
-const path = require("node:path");
-const fs = require("fs");
-const Store = require("electron-store");
+const {app, BrowserWindow, Menu, ipcMain, dialog, screen, globalShortcut} = require('electron');
+const path = require('node:path');
+const fs = require('fs');
+const Store = require('electron-store');
 const store = new Store();
-const ChildProcess = require("child_process");
+const ChildProcess = require('child_process');
 
 if (handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
+	// squirrel event handled and app will exit in 1000ms, so don't do anything else
+	return;
 }
 
 function handleSquirrelEvent() {
-    if (process.argv.length === 1) {
-        return false;
-    }
+	if (process.argv.length === 1) {
+		return false;
+	}
 
-    const appFolder = path.resolve(process.execPath, "..");
-    console.log("appFolder: "+appFolder);
-    const rootAtomFolder = path.resolve(appFolder, "..");
-    console.log("rootAtomFolder: "+rootAtomFolder);
-    const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
-    console.log("updateDotExe: "+updateDotExe);
-    const exeName = path.basename(process.execPath);
-    console.log("exeName: "+exeName);
+	const appFolder = path.resolve(process.execPath, '..');
+	console.log('appFolder: ' + appFolder);
+	const rootAtomFolder = path.resolve(appFolder, '..');
+	console.log('rootAtomFolder: ' + rootAtomFolder);
+	const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+	console.log('updateDotExe: ' + updateDotExe);
+	const exeName = path.basename(process.execPath);
+	console.log('exeName: ' + exeName);
 
-    const spawn = function(command, args) {
-        let spawnedProcess;
+	const spawn = function (command, args) {
+		let spawnedProcess;
 
-        try {
-            spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-        } catch (error) {
-            console.error("Error: "+ error);
-        }
+		try {
+			spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+		} catch (error) {
+			console.error('Error: ' + error);
+		}
 
-        return spawnedProcess;
-    };
+		return spawnedProcess;
+	};
 
-    const spawnUpdate = function(args) {
-        return spawn(updateDotExe, args);
-    };
+	const spawnUpdate = function (args) {
+		return spawn(updateDotExe, args);
+	};
 
-    const squirrelEvent = process.argv[1];
-    switch (squirrelEvent) {
-    case "--squirrel-install":
-        return;
-    case "--squirrel-updated":
-        // Optionally do things such as:
-        // - Add your .exe to the PATH
-        // - Write to the registry for things like file associations and
-        //   explorer context menus
+	const squirrelEvent = process.argv[1];
+	switch (squirrelEvent) {
+		case '--squirrel-install':
+			return;
+		case '--squirrel-updated':
+			// Optionally do things such as:
+			// - Add your .exe to the PATH
+			// - Write to the registry for things like file associations and
+			//   explorer context menus
 
-        // Install desktop and start menu shortcuts
-        spawnUpdate(["--createShortcut", exeName]);
+			// Install desktop and start menu shortcuts
+			spawnUpdate(['--createShortcut', exeName]);
 
-        setTimeout(app.quit, 1000);
-        return true;
+			setTimeout(app.quit, 1000);
+			return true;
 
-    case "--squirrel-uninstall":
-        // Undo anything you did in the --squirrel-install and
-        // --squirrel-updated handlers
+		case '--squirrel-uninstall':
+			// Undo anything you did in the --squirrel-install and
+			// --squirrel-updated handlers
 
-        // Remove desktop and start menu shortcuts
-        spawnUpdate(["--removeShortcut", exeName]);
+			// Remove desktop and start menu shortcuts
+			spawnUpdate(['--removeShortcut', exeName]);
 
-        setTimeout(app.quit, 1000);
-        return true;
+			setTimeout(app.quit, 1000);
+			return true;
 
-    case "--squirrel-obsolete":
-        // This is called on the outgoing version of your app before
-        // we update to the new version - it's the opposite of
-        // --squirrel-updated
+		case '--squirrel-obsolete':
+			// This is called on the outgoing version of your app before
+			// we update to the new version - it's the opposite of
+			// --squirrel-updated
 
-        app.quit();
-        return true;
-    }
+			app.quit();
+			return true;
+	}
 }
 
-function createWindow () {
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: width,
-        height: height,
-        icon: path.join(__dirname, "favicon.ico"),
-        webPreferences: {
-            zoomFactor: 1.5,
-            contextIsolation: false,
-            nodeIntegration: true,
-        },
-    });
+function createWindow() {
+	const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: width,
+		height: height,
+		icon: path.join(__dirname, 'favicon.ico'),
+		webPreferences: {
+			zoomFactor: 1.5,
+			contextIsolation: false,
+			nodeIntegration: true,
+		},
+	});
 
-    // and load the index.html of the app.
-    mainWindow.loadFile("ng/dist/browser/index.html");
+	// and load the index.html of the app.
+	mainWindow.loadFile('ng/dist/browser/index.html');
 
-    // Open the DevTools.
-    /* mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	/* mainWindow.webContents.openDevTools();
     mainWindow.webContents.on("did-frame-finish-load", () => {
         mainWindow.webContents.setZoomFactor(1.5); 
     }); */
 
-    // Registra un atajo de teclado global para abrir las herramientas de desarrollo
-    globalShortcut.register("CommandOrControl+Alt+I", () => {
-    // Abre las herramientas de desarrollo
-        mainWindow.webContents.openDevTools();
-        mainWindow.webContents.on("did-frame-finish-load", () => {
-            mainWindow.webContents.setZoomFactor(1.5); 
-        });
-    });
+	// Registra un atajo de teclado global para abrir las herramientas de desarrollo
+	globalShortcut.register('CommandOrControl+Alt+I', () => {
+		// Abre las herramientas de desarrollo
+		mainWindow.webContents.openDevTools();
+		mainWindow.webContents.on('did-frame-finish-load', () => {
+			mainWindow.webContents.setZoomFactor(1.5);
+		});
+	});
 
-    ipcMain.on("openDialog", () => {
-        dialog.showOpenDialog(mainWindow, {
-            properties: ["openDirectory"]
-        }).then(async (result) => {
-            console.log("RUTA: "+result.filePaths);
-            store.set("carpetaSeleccionada", result.filePaths[0]);
-            if (!result.canceled && result.filePaths.length > 0) {
-                const folderPath = result.filePaths[0];
-                const files = await readFilesInFolder(folderPath);
-                mainWindow.webContents.send("archivos-de-carpeta", files);
-            }
-        });
-    });
+	mainWindow.ipcMain.on('openDialog', () => {
+		dialog
+			.showOpenDialog(mainWindow, {
+				properties: ['openDirectory'],
+			})
+			.then(async (result) => {
+				console.log('RUTA: ' + result.filePaths);
+				store.set('carpetaSeleccionada', result.filePaths[0]);
+				if (!result.canceled && result.filePaths.length > 0) {
+					const folderPath = result.filePaths[0];
+					const files = await readFilesInFolder(folderPath);
+					mainWindow.webContents.send('archivos-de-carpeta', files);
+				}
+			});
+	});
 
-    ipcMain.on("DialogCSV", () => {
-        dialog.showOpenDialog(mainWindow, {
-            properties: ["openFile", "showHiddenFiles"],
-            filters: [{ name: "CSV", extensions: ["csv", "CSV"] },]
-        }).then((result) => {
-            console.log("CSV: "+result.filePaths);
-            store.set("CSV",result.filePaths[0]);
-            const file = fs.readFileSync(result.filePaths[0]).toString();
-            mainWindow.webContents.send("CSV", file);
-        }).catch(err => console.error("Error en DialogCSV: \n"+err));
-    });
+	ipcMain.on('DialogCSV', () => {
+		dialog
+			.showOpenDialog(mainWindow, {
+				properties: ['openFile', 'showHiddenFiles'],
+				filters: [{name: 'CSV', extensions: ['csv', 'CSV']}],
+			})
+			.then((result) => {
+				console.log('CSV: ' + result.filePaths);
+				store.set('CSV', result.filePaths[0]);
+				const file = fs.readFileSync(result.filePaths[0]).toString();
+				mainWindow.webContents.send('CSV', file);
+			})
+			.catch((err) => console.error('Error en DialogCSV: \n' + err));
+	});
 
-    ipcMain.on("dialogStatus", () => {
-        dialog.showOpenDialog(mainWindow, {
-            properties: ["openFile", "showHiddenFiles"],
-            filters: [{ name: "Status", extensions: ["status", "STATUS", "Status"] },]
-        }).then((result) => {
-            store.set("Status", result.filePaths[0]);
-            const file = fs.readFileSync(result.filePaths[0]).toString();
-            mainWindow.webContents.send("StatusCSV", file);
-        }).catch(err => console.error("Error en dialogStatus: \n"+err));
-    });
+	ipcMain.on('dialogStatus', () => {
+		dialog
+			.showOpenDialog(mainWindow, {
+				properties: ['openFile', 'showHiddenFiles'],
+				filters: [{name: 'Status', extensions: ['status', 'STATUS', 'Status']}],
+			})
+			.then((result) => {
+				store.set('Status', result.filePaths[0]);
+				const file = fs.readFileSync(result.filePaths[0]).toString();
+				mainWindow.webContents.send('StatusCSV', file);
+			})
+			.catch((err) => console.error('Error en dialogStatus: \n' + err));
+	});
 
-    ipcMain.on("PersistenciaCarpeta", () => {
-        const storedFolderPath = store.get("carpetaSeleccionada");
-        if(storedFolderPath && storedFolderPath.length > 0) {
-            let files = fs.readdirSync(storedFolderPath);
-            let result = [];
-            for(let file of files) {
-                result.push(storedFolderPath+"/"+file);
-            }
-            mainWindow.webContents.send("Carpeta", result);
-        }
-    });
+	ipcMain.on('PersistenciaCarpeta', () => {
+		const storedFolderPath = store.get('carpetaSeleccionada');
+		if (storedFolderPath && storedFolderPath.length > 0) {
+			let files = fs.readdirSync(storedFolderPath);
+			let result = [];
+			for (let file of files) {
+				result.push(storedFolderPath + '/' + file);
+			}
+			mainWindow.webContents.send('Carpeta', result);
+		}
+	});
 
-    ipcMain.on("PersistenciaCSV", () => {
-        const CSVGuardado = store.get("CSV");
-        if(CSVGuardado && CSVGuardado.length > 0) {
-            const file = fs.readFileSync(CSVGuardado).toString();
-            mainWindow.webContents.send("CSVRecuperado", file);
-        }
-    });
+	ipcMain.on('PersistenciaCSV', () => {
+		const CSVGuardado = store.get('CSV');
+		if (CSVGuardado && CSVGuardado.length > 0) {
+			const file = fs.readFileSync(CSVGuardado).toString();
+			mainWindow.webContents.send('CSVRecuperado', file);
+		}
+	});
 
-    ipcMain.on("persistenciaStatus", () => {
-        console.log("persistencia status");
-        const StatusGuardado = store.get("Status");
-        console.log("statusguardado: "+StatusGuardado);
-        if(StatusGuardado !== undefined && StatusGuardado.length > 0) {
-            const file = fs.readFileSync(StatusGuardado).toString();
-            console.log("file: "+file);
-            mainWindow.webContents.send("StatusRecuperado", file);
-        }else{
-            mainWindow.webContents.send("StatusRecuperado", null);
-        }
-    });
+	ipcMain.on('persistenciaStatus', () => {
+		console.log('persistencia status');
+		const StatusGuardado = store.get('Status');
+		console.log('statusguardado: ' + StatusGuardado);
+		if (StatusGuardado !== undefined && StatusGuardado.length > 0) {
+			const file = fs.readFileSync(StatusGuardado).toString();
+			console.log('file: ' + file);
+			mainWindow.webContents.send('StatusRecuperado', file);
+		} else {
+			mainWindow.webContents.send('StatusRecuperado', null);
+		}
+	});
 
-    ipcMain.on("busca", (_evento, ruta) => {
-        let file = fs.readFileSync(ruta);
-        let arraybuffer = file.buffer.slice(
-            file.byteOffset,
-            file.byteOffset + file.byteLength
-        );
-        mainWindow.webContents.send("arraybuffer",arraybuffer);
-    });
+	ipcMain.on('busca', (_evento, ruta) => {
+		let file = fs.readFileSync(ruta);
+		let arraybuffer = file.buffer.slice(file.byteOffset, file.byteOffset + file.byteLength);
+		mainWindow.webContents.send('arraybuffer', arraybuffer);
+	});
 
-    ipcMain.on("addStatus", (_event, data) => {
-        let ruta = store.get("Status");
-        if(ruta) {
-            let statusString = fs.readFileSync(ruta).toString();
-            let nuevoStatus = data+"\n"+statusString;
-            fs.writeFileSync(ruta,nuevoStatus);
-        }else{
-            let nuevoStatus = data;
-            let userDataPath = app.getPath("userData");
-            let statusFilePath = `${userDataPath}/status.status`;
-            fs.writeFileSync(statusFilePath, nuevoStatus);
-            store.set("Status",statusFilePath);
-        }  
-    });
+	ipcMain.on('addStatus', (_event, data) => {
+		let ruta = store.get('Status');
+		if (ruta) {
+			let statusString = fs.readFileSync(ruta).toString();
+			let nuevoStatus = data + '\n' + statusString;
+			fs.writeFileSync(ruta, nuevoStatus);
+		} else {
+			let nuevoStatus = data;
+			let userDataPath = app.getPath('userData');
+			let statusFilePath = `${userDataPath}/status.status`;
+			fs.writeFileSync(statusFilePath, nuevoStatus);
+			store.set('Status', statusFilePath);
+		}
+	});
 
-    async function readFilesInFolder(folderPath) {
-        const files = [];
-        const filenames = fs.readdirSync(folderPath);
-  
-        for (const filename of filenames) {
-            const filePath = path.join(folderPath, filename);
-            const fileBuffer = fs.readFileSync(filePath);
-            files.push({
-                name: filename,
-                ruta: filePath,
-                buffer: fileBuffer
-            });
-        }
-        return files;
-    }
+	async function readFilesInFolder(folderPath) {
+		const files = [];
+		const filenames = fs.readdirSync(folderPath);
+
+		for (const filename of filenames) {
+			const filePath = path.join(folderPath, filename);
+			const fileBuffer = fs.readFileSync(filePath);
+			files.push({
+				name: filename,
+				ruta: filePath,
+				buffer: fileBuffer,
+			});
+		}
+		return files;
+	}
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow();
+	createWindow();
 
-    app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
-ipcMain.on("Files", (event, files) => {
-    console.log("Recibido en Electron: "+typeof files);
-}); 
-
-
+ipcMain.on('Files', (event, files) => {
+	console.log('Recibido en Electron: ' + typeof files);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
@@ -251,5 +254,3 @@ app.on("window-all-closed", function () {
 
 //const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(null);
-
-
--- ng/src/main.ts
+++ ng/src/main.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
-import { routes } from './app/app.routes';
+import {AppComponent} from './app/app.component';
+import {routes} from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
 	providers: [provideProtractorTestingSupport(), provideRouter(routes)],
@@ -9,8 +9,23 @@ bootstrapApplication(AppComponent, {
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 	document.body.classList.add('modo-oscuro');
-	//cambiaEstilo.textContent = 'Modo Claro';
+	document.documentElement.classList.add('modo-oscuro');
 }
 
+// Slider modo oscuro en Angular
+const observer = new MutationObserver((mutations) => {
+	mutations.forEach((mutation) => {
+		if (mutation.attributeName === 'class') {
+			if (document.body.classList.contains('modo-oscuro')) {
+				document.documentElement.classList.add('modo-oscuro');
+			} else {
+				document.documentElement.classList.remove('modo-oscuro');
+			}
+		}
+	});
+});
 
-  
+// Comenzar a observar el elemento <body> para cambios en los atributos
+observer.observe(document.body, {
+	attributes: true, // Observar cambios en los atributos
+});
