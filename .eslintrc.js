65b01b8e58a089ee9ca4fdd8477aa0a4753125ce
Giacca90 <giacca90mobile@gmail.com>
2024-09-24 18:05:05 +0200

    Movido el proyecto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
--- forge.config.js
+++ forge.config.js
@@ -1,43 +1,43 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    derefSymlinks: true,
-    icon: 'favicon.ico'
-  },
-  rebuildConfig: {},
+	packagerConfig: {
+		asar: true,
+		derefSymlinks: true,
+		icon: 'favicon.ico',
+	},
+	rebuildConfig: {},
 
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD,
-        authors: 'Giacca90',
-        description: 'App desktop de rellenado de plantillas',
-        setupIcon: 'favicon.ico'
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin','win32'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {
-        iconUrl: 'favicon-linux.png'
-      },
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        iconUrl: 'favicon-linux.png'
-      },
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+	makers: [
+		{
+			name: '@electron-forge/maker-squirrel',
+			config: {
+				certificateFile: './cert.pfx',
+				certificatePassword: process.env.CERTIFICATE_PASSWORD,
+				authors: 'Giacca90',
+				description: 'App desktop de rellenado de plantillas',
+				setupIcon: 'favicon.ico',
+			},
+		},
+		{
+			name: '@electron-forge/maker-zip',
+			platforms: ['darwin', 'win32'],
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {
+				iconUrl: 'favicon-linux.png',
+			},
+		},
+		{
+			name: '@electron-forge/maker-rpm',
+			config: {
+				iconUrl: 'favicon-linux.png',
+			},
+		},
+	],
+	plugins: [
+		{
+			name: '@electron-forge/plugin-auto-unpack-natives',
+			config: {},
+		},
+	],
 };
